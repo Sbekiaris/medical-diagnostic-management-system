c82c85a6dc36833aeb2e63cea3f1f297ad464781
sbekiaris <snbekiaris@gmail.com>
2026-05-05 22:40:04 +0300

    feat: create methods for patients

--- ManagementSystem.java
+++ ManagementSystem.java
@@ -45,26 +45,56 @@ public class ManagementSystem {
         doctors.put(id, doctor);
     }
     
-    // display all doctors
-    public ArrayList<Doctor> displayAllDoctors(){
+    // get all doctors
+    public ArrayList<Doctor> getAllDoctors(){
         return new ArrayList<>(doctors.values());
     }
 
     // get a doctor
-    public Doctor getDoctor(int id){
+    public Doctor getDoctor(int id){    // TODO: 
         return doctors.get(id);
     }
 
     // doctor appointment 
     public ArrayList<Appointment> getDoctorAppointment(int doctorId){
         ArrayList<Appointment> result = new ArrayList<>();
-        for (Appointment a : appointments.values()) {
+        for (Appointment a : appointments.values()){
             Exam e = exams.get(a.getExamId());
-            if (e != null && e.getDoctorID() == doctorId) {
+            if(e != null && e.getDoctorID() == doctorId) {
                 result.add(a);
             }
         }
         return result;
+    }
+
+    // connection between doctor and exam, one doctor has many exams
+    public ArrayList<Exam> getExamsByDoctor(int doctorId){
+        ArrayList<Exam> result = new ArrayList<>();
+        for(Exam e : exams.values()){
+            if(e.getDoctorID() == doctorId){
+                result.add(e);
+            }
+        }
+        return result;
+    }
+
+    // patients methods
+
+    // add patient
+    public void addPatient(String patientName, String patientPhone, String email){  
+        int id = getNextPatientID();
+        Patient patient = new Patient(id, patientName, patientPhone, email);
+        patients.put(id, patient);
+    }
+
+    // get all patients
+    public ArrayList<Patient> getAllPatients(){
+        return new ArrayList<>(patients.values());
+    }
+
+     // get a patient
+    public Patient getPatient(int id){
+        return patients.get(id);
     }
 
 }
